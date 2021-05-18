43c72680bf81f529de24142eeaad13897a6767ab
Ednis Lyra <ednislyra@yahoo.com>
2021-05-18 22:34:48 +0100

    DELETE Client

--- backend/src/main/java/com/ednis/CRUD_dsclient/services/ClientServices.java
+++ backend/src/main/java/com/ednis/CRUD_dsclient/services/ClientServices.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ednis.CRUD_dsclient.dto.ClientDTO;
 import com.ednis.CRUD_dsclient.entities.Client;
 import com.ednis.CRUD_dsclient.repositories.ClientRepository;
+import com.ednis.CRUD_dsclient.services.exceptions.DatabaseException;
 import com.ednis.CRUD_dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -63,6 +66,19 @@ public class ClientServices {
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation!");
+		}
+		
 	}
 	
 }
