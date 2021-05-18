40a4dc24386d05acc3a910d6dd624489bc5cb24a
Ednis Lyra <ednislyra@yahoo.com>
2021-05-18 19:06:31 +0100

    Project Created

---
+++ backend/src/main/java/com/ednis/CRUD_dsclient/CrudDsclientApplication.java
@@ -0,0 +1,13 @@
+package com.ednis.CRUD_dsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDsclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDsclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ednis/CRUD_dsclient/CrudDsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ednis.CRUD_dsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
